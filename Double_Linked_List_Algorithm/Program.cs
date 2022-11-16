d3ba28c0d65896a10ee01e86a79c685646b50a06
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-16 16:53:31 +0700

    Create Class DoubleLinkedList

--- Double_Linked_List_Algorithm/Program.cs
+++ Double_Linked_List_Algorithm/Program.cs
@@ -18,4 +18,27 @@ namespace Double_Linked_List_Algorithm
         public Node next;
         public Node prev;
     }
+
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = (Console.ReadLine());
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+
+            
+        }
+    }
 }
